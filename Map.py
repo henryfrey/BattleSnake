9196d78cd8a9a2e25a2a44b81554a0a71c45b5d4
henryfrey <138237325+henryfrey@users.noreply.github.com>
2024-05-28 12:37:24 +0000

    Ready für ersten Test

--- Map.py
+++ Map.py
@@ -1,41 +1,79 @@
 import math as m
+from typing import List
 import numpy as num
 
 class Map:
     def __init__(self, length):
       self.array = num.zeros((length, length))
+      self.dimension = length
       self.turn = 0
       self.health = 100
       self.size = 3
-      
+      self.snakeInfo : List = [] #[[x,y],length]
+      self.food : List= [] #[x,y]
+      self.head = [0,0]
+
+    def distanceToPoint(self,pointHead, pointDesirable):
+      return abs(pointHead[0] - pointDesirable[0]) + abs(pointHead[1] - pointDesirable[1])
+
     def fillMap(self, request):
       self.turn = request['turn']
       board = request['board']
-      food = board['food']
-      for i in range(len(food)):
-        self.array[food[i]['x']][food[i]['y']] = 10
       mySnake = request['you']
       self.size = mySnake['length']
       self.health = mySnake['health']
       body = mySnake['body']
+      self.head  = [body[0]['x'], body[0]['y']]
       for i in range(len(body)):
-        self.array[body[i]['x']][body[i]['y']] = -m.inf
+        self.array[body[i]['x']][body[i]['y']] = -1000
+
+      
       snakes = board['snakes']
       for i in range(len(snakes)):
         body = snakes[i]['body']
         head = body[0]
+        self.snakeInfo += [[head['x'], head['y']], snakes[i]['length']]
+        for j in range(len(body)-1):
+          self.array[body[i]['x']][body[i]['y']] = -1000
         if snakes[i]['length'] >= self.size:
-          for j in range(len(body)-1):
-            self.array[body[i]['x']][body[i]['y']] = -m.inf
-          self.array[head['x']+1][head['y']] = -m.inf
-          self.array[head['x']-1][head['y']] = -m.inf
-          self.array[head['x']][head['y']+1] = -m.inf
-          self.array[head['x']][head['y']-1] = -m.inf
+          self.array[head['x']+1][head['y']] = -1000
+          self.array[head['x']-1][head['y']] = -1000
+          self.array[head['x']][head['y']+1] = -1000
+          self.array[head['x']][head['y']-1] = -1000
 
+      food = board['food']
+      for i in range(len(food)):
+        closest = True
+        point = [food[i]['x'], food[i]['y']]
+        self.food += [point[0], point[1]]
+        for i in self.snakeInfo:
+          if self.distanceToPoint(i[0], point) < self.distanceToPoint(self.head, point):
+            closest = False
+            break
+          if self.distanceToPoint(i[0], point) == self.distanceToPoint(self.head, point) and self.size <= i[1]:
+            closest = False
+            break
+        if closest:
+          self.array[point[0]][point[1]] = 10
+        
+    def spread(self, point):
+      if 0 <= point[0] <= self.dimension and 0 <= point[1] <= self.dimension and self.array[point[0]][point[1]] == 0:
+        self.array[point[0]+1][point[1]] +=  self.array[point[0]][point[1]]-1
+        self.array[point[0]-1][point[1]] +=  self.array[point[0]][point[1]]-1
+        self.array[point[0]][point[1]+1] +=  self.array[point[0]][point[1]]-1
+        self.array[point[0]][point[1]-1] +=  self.array[point[0]][point[1]]-1
+        self.spread([point[0]+1, point[1]])
+        self.spread([point[0]-1, point[1]])
+        self.spread([point[0], point[1]+1])
+        self.spread([point[0], point[1]-1])
+  
     def evaluateMap(self):
+      for i in self.food:
+        self.spread(i)
+          
+        
 
-    def distanceToPoint(pointHead, pointDesirable):
-      return abs(pointHead[0] - pointDesirable[0]) + abs(pointHead[1] - pointDesirable[1])
+     
       
       
       
--- main.py
+++ main.py
@@ -12,6 +12,7 @@
 
 import random
 import typing
+import Map
 
 
 # info is called when you create your Battlesnake on play.battlesnake.com
@@ -43,51 +44,25 @@ def end(game_state: typing.Dict):
 # Valid moves are "up", "down", "left", or "right"
 # See https://docs.battlesnake.com/api/example-move for available data
 def move(game_state: typing.Dict) -> typing.Dict:
-
-    is_move_safe = {"up": True, "down": True, "left": True, "right": True}
-
-    # We've included code to prevent your Battlesnake from moving backwards
-    my_head = game_state["you"]["body"][0]  # Coordinates of your head
-    my_neck = game_state["you"]["body"][1]  # Coordinates of your "neck"
-
-    if my_neck["x"] < my_head["x"]:  # Neck is left of head, don't move left
-        is_move_safe["left"] = False
-
-    elif my_neck["x"] > my_head["x"]:  # Neck is right of head, don't move right
-        is_move_safe["right"] = False
-
-    elif my_neck["y"] < my_head["y"]:  # Neck is below head, don't move down
-        is_move_safe["down"] = False
-
-    elif my_neck["y"] > my_head["y"]:  # Neck is above head, don't move up
-        is_move_safe["up"] = False
-
-    # TODO: Step 1 - Prevent your Battlesnake from moving out of bounds
-    # board_width = game_state['board']['width']
-    # board_height = game_state['board']['height']
-
-    # TODO: Step 2 - Prevent your Battlesnake from colliding with itself
-    # my_body = game_state['you']['body']
-
-    # TODO: Step 3 - Prevent your Battlesnake from colliding with other Battlesnakes
-    # opponents = game_state['board']['snakes']
-
-    # Are there any safe moves left?
-    safe_moves = []
-    for move, isSafe in is_move_safe.items():
-        if isSafe:
-            safe_moves.append(move)
-
-    if len(safe_moves) == 0:
-        print(f"MOVE {game_state['turn']}: No safe moves detected! Moving down")
-        return {"move": "down"}
-
-    # Choose a random move from the safe ones
-    next_move = random.choice(safe_moves)
-
-    # TODO: Step 4 - Move towards food instead of random, to regain health and survive longer
-    # food = game_state['board']['food']
-
+    map = Map.Map(11)
+    map.fillMap(game_state)
+    map.evaluateMap()
+    my_head = map.head
+    moves = []
+    if my_head[0] > 0:
+        moves.append([map.array[my_head[0]-1][my_head[1]], "left"])
+    if my_head[0] < map.dimension:
+        moves.append([map.array[my_head[0]+1][my_head[1]], "right"])
+    if my_head[1] > 0:
+        moves.append([map.array[my_head[0]][my_head[1]-1], "down"])
+    if my_head[1] < map.dimension:
+        moves.append([map.array[my_head[0]][my_head[1]+1], "up"])
+    while len(moves) > 1:
+        if moves[0][0] <= moves[1][0]:
+            moves.pop(0)
+        else:
+            moves.pop(1)
+    next_move = moves[0][1]
     print(f"MOVE {game_state['turn']}: {next_move}")
     return {"move": next_move}
 
